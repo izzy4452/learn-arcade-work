9e4a090f2e4d7d140954433ddfede2bffeeadc46
israelvelazquez <v.israel199@gmail.com>
2022-01-22 07:04:27 -0700

    Homework 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-hello
+print("Hello my name is Israel!\n")
+print("I am excited to be taking this class.")
+print("It is going to be awesome to build a game using python.")
+print("Thank you for your time!")
